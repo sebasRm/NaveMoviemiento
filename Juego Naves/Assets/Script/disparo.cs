5272d6c7aff4cddf23feebefb49fc614f6ea0d49
sebasRm <71810357+sebasRm@users.noreply.github.com>
2021-10-07 11:51:05 -0500

    Movimiento y disparo nave
    se envia el moviemiento de la nave junto con los ataques de desiparo

---
+++ Juego Naves/Assets/Script/disparo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disparo : MonoBehaviour
+{
+    [SerializeField]
+    private float velocidadDisparo = 20.4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * velocidadDisparo * Time.deltaTime);
+
+        if(transform.position.y >= 1.6f)
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
---
+++ Juego Naves/Assets/Script/movimiento.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public string estadoNave = "mi estado";
+
+    public float velocidadNave = 10.0f;
+    public float velocidadNaveLados = 3.0f;
+
+    public float movimientoHorizontal;
+    public float movimientoVertical;
+
+    public int vidasNave = 3;
+
+    public GameObject disparoUno;
+
+    void Start()
+    {
+       // transform.position = new Vector3(0.614f, -0.463f, -2.35f);
+      //  Debug.Log(estadoNave);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        moviemintoNave();
+
+        disparo();
+
+
+
+    }
+
+
+    void moviemintoNave()
+    {
+        movimientoHorizontal = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * velocidadNaveLados * movimientoHorizontal);
+
+        movimientoVertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * Time.deltaTime * velocidadNave * movimientoVertical);
+
+        if (transform.position.x <= -1.6)
+        {
+            transform.position = new Vector3(-1.6f, transform.position.y, -1.6f);
+        }
+        else if (transform.position.x >= 2.86)
+        {
+            transform.position = new Vector3(2.86f, transform.position.y, -1.6f);
+        }
+
+        else if (transform.position.y >= 1.49)
+        {
+            transform.position = new Vector3(transform.position.x, 1.49f, -1.6f);
+        }
+        else if (transform.position.y <= -0.5)
+        {
+            transform.position = new Vector3(transform.position.x, -0.5f, -1.6f);
+        }
+    }
+
+
+    void disparo()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Instantiate(disparoUno, transform.position + new Vector3(0, 0.5f, -1.16f), Quaternion.identity);
+        }
+        else if(Input.GetKey(KeyCode.T))
+        {
+            Instantiate(disparoUno, transform.position + new Vector3(0, 0.5f, -1.16f), Quaternion.identity);
+            Instantiate(disparoUno, transform.position + new Vector3(0.2f, 0.1f, -1.16f), Quaternion.identity);
+            Instantiate(disparoUno, transform.position + new Vector3(-0.2f, 0.1f, -1.16f), Quaternion.identity);
+        }
+    }
+
+}
